0587145321b511eed6f8a1960083f78ccdbdcce5
BoomGit <631903189@qq.com>
2017-06-28 18:40:34 +0800

    test teamcity

--- GradeView/app/src/main/java/com/boom/gradeview/GradeView.java
+++ GradeView/app/src/main/java/com/boom/gradeview/GradeView.java
@@ -14,6 +14,7 @@ import android.view.View;
  */
 
 public class GradeView extends View {
+
     private  Bitmap bigger_grade_fcous;
     private Bitmap bigger_grade_normal;
     private int mTotal=5;
